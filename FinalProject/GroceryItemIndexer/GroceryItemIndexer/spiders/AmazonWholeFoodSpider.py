e0a2fb8e354534a192e42480b9627f27839b5ca1
Henri <hvandersleyen@gmail.com>
2020-11-19 23:37:36 -0800

    MongoDB works, able to loop through categories of websites. Once we can do it with everycategory we can worry about proxies. Do a run tomorrow

--- FinalProject/GroceryItemIndexer/GroceryItemIndexer/spiders/AmazonWholeFoodSpider.py
+++ FinalProject/GroceryItemIndexer/GroceryItemIndexer/spiders/AmazonWholeFoodSpider.py
@@ -12,17 +12,24 @@ class AmazonWholeFood(scrapy.Spider):
     # variable name required to run scrtapy crawl
     name = 'AWFSpider'
     started = False
-    pageNumber = 7
-    foodSection = 'Gourmet Gifts'
-    start_urls = [f'https://www.amazon.ca/s?rh=n%3A6967215011&page={pageNumber}&qid=1605815840&ref=lp_6967215011_pg_{pageNumber}']
-    
-    # def start_requests(self,response=''): <- where the issue is rn
+    pageNumber = 1
+    foodSection = 'Baby-Food'
+    #start_urls = [f'https://www.amazon.ca/s?rh=n%3A6967215011&page={pageNumber}&qid=1605815840&ref=lp_6967215011_pg_{pageNumber}']
+    start_urls = [f'https://www.Amazon.ca/s?i=grocery&bbn=6967216011&rh=n%3A6967215011%2Cn%3A7351088011&dc&qid=1605856107&rnid=6967216011&ref=sr_nr_n_1']
+    idxKey = 0
+    start_url = ''
+
+    # def start_requests(self): #<- where the issue is rn
     #     # we want to scrape the first page
     #     if not AmazonWholeFood.started:
     #         AmazonWholeFood.started = True
-    #     yield {
-    #         scrapy.Request(url=AmazonWholeFood.start_urls[0] ,callback=self.parse)
-    #     }
+    #         yield {
+    #         scrapy.Request(url=AmazonWholeFood.startUrl[0],callback=self.parse)
+    #         }
+        #else:
+
+    #def 
+
     #
         # else:
         #     catIndex = categorieUrls(response)
@@ -47,26 +54,28 @@ class AmazonWholeFood(scrapy.Spider):
 
         # manual check that the 200 reponse returned  
         self.logger.info(f'\n----------------------------------------start page: {AmazonWholeFood.pageNumber} -- for: {response.url} is ready---------- \n')
-        
-        for i in range(0,len(response.css('.a-color-base.a-text-normal::text').getall())):
-            try:
-                self.logger.info(f'item number:{i} of page:{AmazonWholeFood.pageNumber} \n')
-                groceryItem['name'] = response.css('.a-color-base.a-text-normal::text')[i].getall().pop()
-                #getall() returns a list containing strings. so we pop the element
-                groceryItem['price'] = float((response.css('.a-price span .a-price-whole::text')[i].getall().pop()) + 
-                    '.' + response.css('.a-price span .a-price-fraction::text')[i].getall().pop())
-                groceryItem['currency'] = response.css('.a-price-symbol::text')[i].getall().pop()
-                groceryItem['rating'] = response.css('.aok-align-bottom .a-icon-alt::text')[i].getall().pop()
-                groceryItem['numberOfRating'] = response.css('.a-size-small .a-size-base::text')[i].getall().pop()
-                groceryItem['link'] = response.css('.s-line-clamp-4 .a-link-normal::attr(href)')[i].getall().pop()
-                groceryItem['spider'] = AmazonWholeFood.name
-                groceryItem['section'] = AmazonWholeFood.foodSection
-                groceryItem['time'] = self.timestampReceival()
-                yield groceryItem
-            # some issue as some items falls outside detectable range
-            except Exception as e:
-                # most common error: IndexError: list index out of range
-                self.logger.info(f'error: {e}')
+        if AmazonWholeFood.pageNumber ==1:
+            pass
+        else:
+            for i in range(0,len(response.css('.a-color-base.a-text-normal::text').getall())):
+                try:
+                    self.logger.info(f'item number:{i} of page:{AmazonWholeFood.pageNumber} \n')
+                    groceryItem['name'] = response.css('.a-color-base.a-text-normal::text')[i].getall().pop()
+                    #getall() returns a list containing strings. so we pop the element
+                    groceryItem['price'] = float((response.css('.a-price span .a-price-whole::text')[i].getall().pop()) + 
+                        '.' + response.css('.a-price span .a-price-fraction::text')[i].getall().pop())
+                    groceryItem['currency'] = response.css('.a-price-symbol::text')[i].getall().pop()
+                    groceryItem['rating'] = response.css('.aok-align-bottom .a-icon-alt::text')[i].getall().pop()
+                    groceryItem['numberOfRating'] = response.css('.a-size-small .a-size-base::text')[i].getall().pop()
+                    groceryItem['link'] = response.css('.s-line-clamp-4 .a-link-normal::attr(href)')[i].getall().pop()
+                    groceryItem['spider'] = AmazonWholeFood.name
+                    groceryItem['section'] = AmazonWholeFood.foodSection
+                    groceryItem['time'] = self.timestampReceival()
+                    yield groceryItem
+                # some issue as some items falls outside detectable range
+                except Exception as e:
+                    # most common error: IndexError: list index out of range
+                    self.logger.info(f'error: {e}')
 
         self.logger.info(f'-------------------------------------------end page: {AmazonWholeFood.pageNumber}--')
         
@@ -75,22 +84,42 @@ class AmazonWholeFood(scrapy.Spider):
         #delay for each robots since scrapy is asynchronous and can process up to 32 parse/spiders
         time.sleep(random.randint(1, 5))
         
+
+        
+        # skipping page 1
+        if AmazonWholeFood.pageNumber == 1:
+            AmazonWholeFood.pageNumber += 1
+            next_page = 'https://www.Amazon.ca' + response.css('#pagn :nth-child(3) a::attr(href)').get()
+            self.logger.info(f'------ This is page one Skip------')
+
+            self.logger.info(f'-------Moving to page: {AmazonWholeFood.pageNumber} of {next_page}')
+            
+            yield scrapy.Request(next_page,callback=self.parse)
+
+        # we scraped the entire section and we want to move on
         # loop from page 2 to the end
         #if AmazonWholeFood.next_page is not None:
-        
-        # test if
-        if AmazonWholeFood.pageNumber < 3:
+        elif 1 < AmazonWholeFood.pageNumber <3:
             AmazonWholeFood.pageNumber += 1
             next_page = 'https://www.Amazon.ca' + response.css('.a-last a::attr(href)').get()
-            self.logger.info(f"{response.css('.a-last a::attr(href)').get()}")
+
             self.logger.info(f'-------Moving to page: {AmazonWholeFood.pageNumber} of {next_page}')
             # error above
             yield scrapy.Request(next_page,callback=self.parse)
-        # we scraped the entire section
+
+        # category change
         else:
+            AmazonWholeFood.pageNumber = 1
+            catIndex = self.categorieUrls(response)
+            AmazonWholeFood.idxKey += 1
+            AmazonWholeFood.start_urls = catIndex[AmazonWholeFood.idxKey][0] 
+            AmazonWholeFood.foodSection = catIndex[AmazonWholeFood.idxKey][1] 
+
+            self.logger.info(f'\n New category \n Index -- {AmazonWholeFood.idxKey} \n-- New Category:{AmazonWholeFood.foodSection} \n-- New url: {AmazonWholeFood.start_urls}\n \n')
+            yield scrapy.Request(AmazonWholeFood.start_urls,callback=self.parse)
             # some program that move to another list category
             # return start_requests(response)
-            pass
+            # pass
 
 
 
@@ -140,7 +169,7 @@ class AmazonWholeFood(scrapy.Spider):
         urlCategory = dict()
         for i in range(len(urlReady)):
 
-            urlCategory[catList[i]] = urlReady[i]
+            urlCategory[i] = (urlReady[i],catList[i])
         return urlCategory
 
 # # finding a way to loop through website
